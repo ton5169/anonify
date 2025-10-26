3e9816bd7b819d6b9b35f6082ffbeec7ed7cf8df
Anton Irshenko <irshenko@gmail.com>
2025-10-26 20:15:18 +0100

    sanity

--- backend/app/api/v1/routes/__init__.py
+++ backend/app/api/v1/routes/__init__.py
@@ -1,8 +1,6 @@
 from app.api.v1.routes.ping import router as ping_router
-from app.api.v1.routes.sanitizer import router as sanitizer_router
 from fastapi import APIRouter
 
 router = APIRouter()
 
 router.include_router(ping_router, prefix="/ping", tags=["ping"])
-router.include_router(sanitizer_router, prefix="/sanitizer", tags=["sanitizer"])
